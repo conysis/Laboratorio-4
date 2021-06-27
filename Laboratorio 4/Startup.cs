c4159a106c935fd41c789b0ade2819a7e9d7ec47
Alexander Goicoechea <goico@alexandergoicoechea.com>
2021-06-27 01:38:51 -0600

    Testing

---
+++ Laboratorio 4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Laboratorio_4.Startup))]
+namespace Laboratorio_4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
